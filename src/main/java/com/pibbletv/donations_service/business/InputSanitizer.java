e60090ca923252c4c071542cf7979e6560f647bc
TanzerDx <hristoganchev3@gmail.com>
2025-06-17 12:16:35 +0200

    test

--- src/main/java/com/pibbletv/donations_service/business/InputSanitizer.java
+++ src/main/java/com/pibbletv/donations_service/business/InputSanitizer.java
@@ -3,7 +3,7 @@ package com.pibbletv.donations_service.business;
 public class InputSanitizer {
 
     public static String sanitizeSearchTerm(String input) {
-        if (input == null || input.length() > 100 || !input.matches("[a-zA-Z0-9\\s\\-]{1,50}")) {
+        if (input == null || input.length() > 100 || !input.matches("[a-zA-Z0-9\\s\\-!,.?']{1,200}")) {
             throw new IllegalArgumentException("Invalid input");
         }
         return input.trim();
